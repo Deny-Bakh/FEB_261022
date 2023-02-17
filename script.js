072fe61118633441a3d6f719fca3f2c96478f020
Denis Bakhmachuk <dennis_bakh@Deniss-MacBook-Air.local>
2023-02-17 13:51:03 +0200

    wip

--- script.js
+++ script.js
@@ -6,66 +6,66 @@ $(document).ready(function() {
     })
 })
 
-const mainPage = 'index.html';
-if (window.location.href.includes(mainPage)){
-  function scrollToElem(elem) {
-    event.preventDefault()
-    const target = document.querySelector(elem.hash)
-    window.scrollTo({
-      top: target.offsetTop,
-      behavior: "smooth"
-    })
-  };
+// const mainPage = 'index.html';
+// if (window.location.href.includes(mainPage)){
+//   function scrollToElem(elem) {
+//     event.preventDefault()
+//     const target = document.querySelector(elem.hash)
+//     window.scrollTo({
+//       top: target.offsetTop,
+//       behavior: "smooth"
+//     })
+//   };
 
-  $('#firstSlider').slick({
-    dots: true,
-    // autoplay: true,
-    // autoplaySpeed: 4000,
-    arrows:false,
-    // pauseOnHover:true,
-    // pauseOnDotsHover:true,
-  });
+//   $('#firstSlider').slick({
+//     dots: true,
+//     // autoplay: true,
+//     // autoplaySpeed: 4000,
+//     arrows:false,
+//     // pauseOnHover:true,
+//     // pauseOnDotsHover:true,
+//   });
 
-  function setupNavtabs(navtab) {
-    var tabs = navtab.querySelectorAll("*[data-target]");
-    for (var i=0; i < tabs.length; i++) {
-      var tab = tabs[i];
+//   function setupNavtabs(navtab) {
+//     var tabs = navtab.querySelectorAll("*[data-target]");
+//     for (var i=0; i < tabs.length; i++) {
+//       var tab = tabs[i];
   
-      tab.addEventListener("click", function(e) {
-        var id = e.target.dataset.target;
-        var pane = document.getElementById(id);
-        pane.classList.add("active");
+//       tab.addEventListener("click", function(e) {
+//         var id = e.target.dataset.target;
+//         var pane = document.getElementById(id);
+//         pane.classList.add("active");
   
-        // remove .active class of all sibling elements
-        var el = pane.nextElementSibling;
-        while (el) {
-          el.classList.remove("active");
-          el = el.nextElementSibling;
-        }
-        el = pane.previousElementSibling;
-        while (el) {
-          el.classList.remove("active");
-          el = el.previousElementSibling;
-        }
-      })
-    }
-  };
+//         // remove .active class of all sibling elements
+//         var el = pane.nextElementSibling;
+//         while (el) {
+//           el.classList.remove("active");
+//           el = el.nextElementSibling;
+//         }
+//         el = pane.previousElementSibling;
+//         while (el) {
+//           el.classList.remove("active");
+//           el = el.previousElementSibling;
+//         }
+//       })
+//     }
+//   };
   
-  var navtabs = document.querySelectorAll(".nav-tabs");
-  for (var i=0; i < navtabs.length; i++) {
-    setupNavtabs(navtabs[i]);
-  }
+//   var navtabs = document.querySelectorAll(".nav-tabs");
+//   for (var i=0; i < navtabs.length; i++) {
+//     setupNavtabs(navtabs[i]);
+//   }
 
-  const navBar = document.querySelector('.nav-tabs').querySelectorAll("a");
-  navBar.forEach(element => {
-    element.addEventListener("click", function(){
-      navBar.forEach(nav=>nav.classList.remove ("active-bg"))
+//   const navBar = document.querySelector('.nav-tabs').querySelectorAll("a");
+//   navBar.forEach(element => {
+//     element.addEventListener("click", function(){
+//       navBar.forEach(nav=>nav.classList.remove ("active-bg"))
 
-      this.classList.add("active-bg");
-    })
-  });
+//       this.classList.add("active-bg");
+//     })
+//   });
 
-};
+// };
 
 const aboutUs = 'about_us.html';
 if (window.location.href.includes(aboutUs)){
